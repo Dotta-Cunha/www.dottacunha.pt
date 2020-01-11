942091c6585566403b9eb8c90fb8ee49b369a3d6
Diogo Cunha <diogo.f.cunha@gmail.com>
2020-01-11 17:34:44 +0000

    Disable viewport zoom

--- src/layouts/Page.tsx
+++ src/layouts/Page.tsx
@@ -15,7 +15,7 @@ export const Page = ({ children }: PageProps) => (
   <>
     <Helmet
       title='Dotta &amp; Cunha'
-      meta={[]}
+      meta={[{ name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' }]}
       link={[{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Megrim&display=swap' }]}
     />
     <Global styles={globalStyles} />
